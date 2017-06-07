cfb8dca0b48ee22db76184f3970dfe0435beea88
Tan Zhong Yan <iam@tzhongyan.com>
2017-06-07 10:31:40 +0800

    removing auto remove function from autoplaylist

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -420,9 +420,11 @@ class MusicBot(discord.Client):
                 info = await self.downloader.safe_extract_info(player.playlist.loop, song_url, download=False, process=False)
 
                 if not info:
+                '''
                     self.autoplaylist.remove(song_url)
                     self.safe_print("[Info] Removing unplayable song from autoplaylist: %s" % song_url)
                     write_file(self.config.auto_playlist_file, self.autoplaylist)
+                '''
                     continue
 
                 if info.get('entries', None):  # or .get('_type', '') == 'playlist'
